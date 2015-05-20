36392534ffe0301c224be936911fa37807144e9f
CodeFlowed <syncstep@gmail.com>
2015-05-20 19:39:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imadedis::Application.config.secret_key_base = '7bcbd9eca2ef4fa645ded982cb4bb216199bd0c8fb2b502e121730dd89ac3eb66636d425f10e8eaa820fd724f009a5d2c5198517ac7e2e5ad7778b35be7985bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imadedis::Application.config.session_store :cookie_store, key: '_imadedis_session'
