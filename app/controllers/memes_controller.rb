ef6b29f5c78af360fc2dd9537b595e1af66d5969
CodeFlowed <syncstep@gmail.com>
2015-05-29 01:50:28 +0000

    Add more CSS styling and modify memes_controller.rb

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,10 +1,20 @@
 class MemesController < ApplicationController
 	def index
-		@memestar = Memestar.first
+		@memestar = Memestar.order("RANDOM()").first
 	end
 
 	def new
 		@memestar = Memestar.new
 	end
 
+	def create
+		Memestar.create(memestar_params)
+		redirect_to memes_path
+	end
+
+	private
+
+	def memestar_params
+		params.require(:memes).permit(:description, :author)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Imadedis::Application.routes.draw do
   root 'memes#index'
 
   resources :memes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
